feaa5aa24312a490b34d7f41e695c579210eeb5f
yawarpwn <john.kazz@gmail.com>
2022-10-14 23:00:16 -0500

    buscador funcionando

--- src/components/search/autocomplete.tsx
+++ src/components/search/autocomplete.tsx
@@ -1,6 +1,15 @@
-import { autocomplete } from '@algolia/autocomplete-js'
+import { autocomplete, getAlgoliaResults } from '@algolia/autocomplete-js'
 import { useEffect, useRef } from 'preact/hooks'
 import { createElement, render, Fragment } from 'preact'
+import ProductItem from './product-item'
+import algoliasearch from 'algoliasearch'
+import '@algolia/autocomplete-theme-classic'
+import './autocomplete.css'
+
+const searchClient = algoliasearch(
+  'EVPE0RYU8N',
+  '14bdd4985f3b412d4dd6ffb765545816'
+)
 
 export default function Autocomplete(props) {
   const containerRef = useRef(null)
@@ -14,10 +23,41 @@ export default function Autocomplete(props) {
       container: containerRef.current,
       placeholder: 'Buscar Producto',
       defaultActiveItemId: 0,
-      renderer: { createElement , Fragment, render },
-      ...props
+      autoFocus: true,
+      renderer: { createElement, Fragment, render },
+      getSources: ({ query }) => [
+        {
+          sourceId: 'products',
+          getItems() {
+            return getAlgoliaResults({
+              searchClient,
+              queries: [
+                {
+                  indexName: 'tellsenales-seguridad',
+                  query
+                }
+              ]
+            })
+          },
+          templates: {
+            header() {
+              return (
+                <Fragment>
+                  <span className='aa-SourceHeaderTitle'>Seguridad</span>
+                  <div className='aa-SourceHeaderLine' />
+                </Fragment>
+              )
+            },
+            item({ item, components }) {
+              return <ProductItem hit={item} components={components} />
+            },
+            noResults() {
+              return 'Producto no encontrado.'
+            }
+          }
+        }
+      ]
     })
-
 
     return () => {
       search.destroy()
--- src/components/search/product-item.tsx
+++ src/components/search/product-item.tsx
@@ -1,10 +1,33 @@
-export default function ProductItem ({hit, components}) {
-  console.log(hit, components)
-  const { name, img1, slug} = hit
+export default function ProductItem({ hit, components }) {
+  const { name, img1, slug } = hit
   return (
-  <a className="flex items center gap-4" href={`/seguridad/${slug}`}>
-      <img className="object-cover" width={20} height={20} src={img1.url} />
-      {name}
+    <a className="aa-ItemLink" href={`/seguridad/${slug}`}>
+      <div className='aa-ItemWrapper'>
+        <div className='aa-ItemContent'>
+          <div className='aa-ItemIcon'>
+            <img src={img1.url} alt={name} width='40' height='40' />
+          </div>
+          <div className='aa-ItemContentBody'>
+            <div className='aa-ItemContentTitle'>
+              <components.Highlight hit={hit} attribute='name' />
+            </div>
+            <div className='aa-ItemContentDescription'>
+              <components.Snippet hit={hit} attribute='description' />
+            </div>
+          </div>
+        </div>
+        <div className='aa-ItemActions'>
+          <button
+            className='aa-ItemActionButton aa-DesktopOnly aa-ActiveOnly'
+            type='button'
+            title='Select'
+          >
+            <svg viewBox='0 0 24 24' width='20' height='20' fill='currentColor'>
+              <path d='M18.984 6.984h2.016v6h-15.188l3.609 3.609-1.406 1.406-6-6 6-6 1.406 1.406-3.609 3.609h13.172v-4.031z' />
+            </svg>
+          </button>
+        </div>
+      </div>
     </a>
   )
 }
