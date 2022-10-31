8f381ef21c9ff57bafed7a28d095737a9c8b9abe
yawarpwn <john.kazz@gmail.com>
2022-10-31 00:06:40 -0500

    mobile table content fixed

--- src/components/right-sidebar/table-of-content-mobile.tsx
+++ src/components/right-sidebar/table-of-content-mobile.tsx
@@ -9,9 +9,15 @@ interface Props {
 const TableOfContent: FunctionComponent<Props> = ({ headings }) => {
 	headings = [{ slug: 'overview', text: 'Sinopsis', depth: 2 }, ...headings]
 	const toc = useRef<HTMLUListElement>()
+	const [open, setOpen] = useState(false)
 	const [currentID, setCurrentId] = useState('overview')
 	const onThisPageID = 'on-this-page-heading'
+	const currentHeading = headings.find(({ slug }) => slug === currentID)
 
+	const onLinkClick = (e) => {
+		setOpen(false)
+		setCurrentId(e.target.getAttribute('href').replace('#', ''))
+	}
 
 	useEffect(() => {
 		if (!toc.current) return
@@ -40,23 +46,45 @@ const TableOfContent: FunctionComponent<Props> = ({ headings }) => {
 
 	}, [toc.current])
 
+
+
 	return (
-		<div>
-			<div>
-				<h2 class='heading' id={onThisPageID} >
-					En está pagina
-				</h2>
-			</div>
+		<details open={open} className='toc-mobile-container' onToggle={(e) => setOpen(e.target.open)}>
+			<summary className="toc-mobile-header">
+				<div class="toc-mobile-header-content">
+					<div className="toc-toggle">
+						<h2 className='heading' id={onThisPageID} >
+							En está pagina
+						</h2>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 1 16 16"
+							width="16"
+							height="16"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M6.22 3.22a.75.75 0 011.06 0l4.25 4.25a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06-1.06L9.94 8 6.22 4.28a.75.75 0 010-1.06z"
+							></path>
+						</svg>
+					</div>
+					{!open && currentHeading?.slug !== 'overview' && (
+						<span className="toc-current-heading">{currentHeading?.text || ''}</span>
+					)}
+				</div>
+			</summary>
 			<ul ref={toc}>
 				{headings.map(({ depth, slug, text }) => (
 					<li className={`header-link depth-${depth} ${currentID === slug ? 'current-header-link' : ''}`.trim()}>
-						<a href={`#${slug}`}>{text}</a>
+						<a href={`#${slug}`} onClick={onLinkClick}>{text}</a>
 					</li>
 				))}
 			</ul>
-		</div>
+		</details>
 	)
 }
 
 
 export default TableOfContent
+
