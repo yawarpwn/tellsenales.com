9ad6117da8d933248db17440a8397b931034894f
yawarpwn <john.kazz@gmail.com>
2022-12-11 16:42:19 -0500

    hero hover card added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,39 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-    darkMode: 'class',
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-        screens: {
-            sm: '650px',
-            // => @media (min-width: 640px) { ... }
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    screens: {
+      sm: '650px',
+      // => @media (min-width: 640px) { ... }
 
-            md: '50em',
-            // => @media (min-width: 768px) { ... }
+      md: '50em',
+      // => @media (min-width: 768px) { ... }
 
-            lg: '1024px',
-            // => @media (min-width: 1024px) { ... }
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
 
-            xl: '1280px',
-            // => @media (min-width: 1280px) { ... }
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
 
-            '2xl': '1400px'
-            // => @media (min-width: 1400px) { ... }
-        },
-        extend: {
-            colors: {
-                primary: 'hsl(262 80% 50%)',
-                'primary-dark': 'hsl(262 80% 40%)',
-                secondary: 'hsl(316 70% 50%)',
-                'secondary-dark': 'hsl(316 70% 40%)',
-                success: 'hsl(158 65% 52%)',
-                warning: 'hsl(43 96% 56%)',
-                error: 'hsl(0 91% 71%)'
-            },
-            fontFamily: {
-                sans: ['Inter', ...defaultTheme.fontFamily.sans]
-            }
-        }
+      '2xl': '1400px'
+      // => @media (min-width: 1400px) { ... }
     },
-    plugins: []
+    extend: {
+      colors: {
+        primary: 'hsl(262 80% 50%)',
+        'primary-dark': 'hsl(262 80% 40%)',
+        secondary: 'hsl(316 70% 50%)',
+        'secondary-dark': 'hsl(316 70% 40%)',
+        success: 'hsl(158 65% 52%)',
+        warning: 'hsl(43 96% 56%)',
+        error: 'hsl(0 91% 71%)'
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  plugins: []
 }
